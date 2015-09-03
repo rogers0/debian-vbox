d19bb4f323108d6f84b753f2c912a7dddc19e075
Roger Shimizu <rogershimizu@gmail.com>
2015-09-03 23:04:49 +0900

    fix for Debian Jessie

--- 3_script_within_guest_os/1_init.sh
+++ 3_script_within_guest_os/1_init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_ROOT=$(readlink -f $(dirname $0))
+
 mdadm -G /dev/md0 -b internal --bitmap-chunk=4096
 mdadm -G /dev/md1 -b internal --bitmap-chunk=4096
-#if ! grep ^DEVICE /etc/mdadm/mdadm.conf &> /dev/null; then
-#	echo 'DEVICE /dev/sda[12]' >> /etc/mdadm/mdadm.conf
-#fi
+if ! grep ^DEVICE /etc/mdadm/mdadm.conf &> /dev/null; then
+	echo 'DEVICE /dev/sda[12]' >> /etc/mdadm/mdadm.conf
+fi
 
 if grep MODULES=most /etc/initramfs-tools/initramfs.conf &> /dev/null; then
 	sed -i /MODULES=/s/most/list/ /etc/initramfs-tools/initramfs.conf
@@ -20,17 +22,29 @@ echo $FS >> /etc/initramfs-tools/modules
 fi
 update-initramfs -u
 
-if ! grep wheezy-backports /etc/apt/sources.list &> /dev/null; then
-	echo 'deb http://httpredir.debian.org/debian wheezy-backports main' >>  /etc/apt/sources.list
-	apt-get update
+if grep wheezy /etc/apt/sources.list &> /dev/null; then
+	if ! grep wheezy-backports /etc/apt/sources.list &> /dev/null; then
+		echo 'deb http://httpredir.debian.org/debian wheezy-backports main' >>  /etc/apt/sources.list
+		echo 'APT::Default-Release "wheezy";' > /etc/apt/apt.conf.d/80-local.conf
+		apt-get update
+	fi
+	apt-get install -t wheezy-backports --no-install-recommends -y linux-image-686-pae
+elif grep jessie /etc/apt/sources.list &> /dev/null; then
+	if ! grep sid /etc/apt/sources.list &> /dev/null; then
+		echo 'deb http://httpredir.debian.org/debian sid main' >>  /etc/apt/sources.list
+		echo 'APT::Default-Release "jessie";' > /etc/apt/apt.conf.d/80-local.conf
+		apt-get update
+	fi
+	apt-get install -t sid --no-install-recommends -y linux-image-686-pae
 fi
 apt-get install --no-install-recommends -y rsync vim-nox vim-tiny-
-apt-get install -t wheezy-backports --no-install-recommends -y linux-image-686-pae
 apt-get autoremove -y
-sed -i 's:exit 0:#mdadm /dev/md0 --re-add /dev/sdb1\n#mdadm /dev/md1 --re-add /dev/sdb2\n\n&:' /etc/rc.local
+if ! grep mdadm /etc/rc.local &> /dev/null; then
+	sed -i 's:exit 0:#mdadm /dev/md0 --re-add /dev/sdb1\n#mdadm /dev/md1 --re-add /dev/sdb2\n\n&:' /etc/rc.local
+fi
 
 if ! grep 3_script_within_guest_os ~/.bashrc &> /dev/null; then
 	echo 'export PATH=$PATH:~/3_script_within_guest_os' >> ~/.bashrc
 fi
 
-sync; sync; sync
+$SCRIPT_ROOT/break_raid1.sh
--- 3_script_within_guest_os/raid1_resync.sh
+++ 3_script_within_guest_os/raid1_resync.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo mdadm /dev/md0 --re-add /dev/sdb1
 mdadm /dev/md0 --re-add /dev/sdb1
+echo mdadm /dev/md1 --re-add /dev/sdb2
 mdadm /dev/md1 --re-add /dev/sdb2
+
 watch -n1 cat /proc/mdstat
