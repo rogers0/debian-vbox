c866f36daf1dd9148a4947ffd6f5e8785ce9bb31
Roger Shimizu <rogershimizu@gmail.com>
2015-10-14 00:47:09 +0900

    make install script more flexible
    the following setting can be changed:
    - RAID or single HDD
    - Network in bridge or NAT mode
    - Memory size (in MiB)
    - Storage size (in MiB)

--- 1_make_debian_vbox.sh
+++ 1_make_debian_vbox.sh
@@ -2,6 +2,10 @@
 
 DISTRO=jessie
 #ARCH=amd64
+RAID=1
+BRIDGE=1
+MEMORY=256
+STORAGE=4096
 
 case "$DISTRO" in
 "wheezy")
@@ -23,28 +27,36 @@ case "$DISTRO" in
 	;;
 esac
 
+OSTYPE=Debian
 if [ $ARCH != "i386" ]; then
 	ISOIMG=${ISOIMG/i386/$ARCH}
 	ISOURL=${ISOURL/i386/$ARCH}; ISOURL=${ISOURL/i386/$ARCH}
+	[ $ARCH = "amd64" ] &&
+		OSTYPE=Debian_64
 fi
 
 [ ! -f "$ISOIMG" ] && wget -O "$ISOIMG" "$ISOURL"
 
-VM=vbox_$DISTRO
+VM=vbox_${DISTRO}
 [ -d "$VM" ] && echo VM "$VM" exists already && exit 1
 
-VBoxManage createvm --name $VM --ostype Debian --basefolder $(pwd) --register
-#BRIDGE=$(/sbin/ifconfig|grep HWaddr|grep -v vboxnet|head -n1|awk '{print $1}')
-#echo Bridge device: $BRIDGE
-#VBoxManage modifyvm $VM --memory=256 --nic1=bridged --bridgeadapter1=$BRIDGE
-VBoxManage modifyvm $VM --memory=256 --nic1=nat
+VBoxManage createvm --name $VM --ostype $OSTYPE --basefolder $(pwd) --register
+if [ $BRIDGE -eq 1 ]; then
+	BRIDGE=$(/sbin/ifconfig|grep HWaddr|grep -v vboxnet|head -n1|awk '{print $1}')
+	echo Bridge device: $BRIDGE
+	VBoxManage modifyvm $VM --memory=$MEMORY --nic1=bridged --bridgeadapter1=$BRIDGE
+else
+	VBoxManage modifyvm $VM --memory=$MEMORY --nic1=nat
+fi
 
 VBoxManage storagectl $VM --add sata --portcount 2 --bootable on --name SATA
 VBoxManage storagectl $VM --add ide --bootable on --name IDE
-VBoxManage createhd --filename $VM/sata_dev --size 4096
-VBoxManage createhd --filename $VM/pata_dev --size 4096
+VBoxManage createhd --filename $VM/sata_dev --size $STORAGE
 VBoxManage storageattach $VM --storagectl SATA --port 0 --type hdd --medium $VM/sata_dev.vdi
-VBoxManage storageattach $VM --storagectl IDE --port 0 --device 0 --type hdd --medium $VM/pata_dev.vdi
+if [ $RAID -eq 1 ]; then
+	VBoxManage createhd --filename $VM/pata_dev --size $STORAGE
+	VBoxManage storageattach $VM --storagectl IDE --port 0 --device 0 --type hdd --medium $VM/pata_dev.vdi
+fi
 VBoxManage storageattach $VM --storagectl IDE --port 1 --device 0 --type dvddrive --medium $ISOIMG
 
 echo now start the VM: $VM
